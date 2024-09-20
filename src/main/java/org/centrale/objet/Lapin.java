5ffa1c31f62d3c6fab5b5a74ed2a5f5df1a10b16
paulmauny <paulmauny@yahoo.fr>
2024-09-20 16:59:41 +0200

    Update Lapin.java

--- src/main/java/org/centrale/objet/Lapin.java
+++ src/main/java/org/centrale/objet/Lapin.java
@@ -2,21 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package org.centrale.objet;
+package org.centrale.objet.WoE;
 
 /**
  *
  * @author barbo
  */
 public class Lapin extends Monstre {
+    
     public Lapin(int ptVie, int degAtt,int ptPar, int pageAtt,int pagePar, Point2D pos){
         super(ptVie,degAtt, ptPar, pageAtt,pagePar, pos);
     }
+    
     public Lapin(Lapin i){
         super(i);
     }
+    
     public Lapin(){
         super();
     }   
-}
 
+}
