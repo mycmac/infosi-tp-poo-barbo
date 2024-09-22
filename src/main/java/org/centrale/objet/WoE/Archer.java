335d8568f7efaa0dae103edf7414b28a71d1e64c
paulmauny <paulmauny@yahoo.fr>
2024-09-23 00:01:07 +0200

    Update Archer.java

--- src/main/java/org/centrale/objet/WoE/Archer.java
+++ src/main/java/org/centrale/objet/WoE/Archer.java
@@ -4,15 +4,18 @@
  */
 package org.centrale.objet.WoE;
 
+import java.util.Random ;
+
 /**
  *
  * @author PRO
  */
 public class Archer extends Personnage {
     private int nbFleches;
+    
 
-    public Archer(String n, int pV, int dA, int pPar, int paAtt, int paPar, int dMax, Point2D p, int nbFleches) {
-        super(n, pV, dA, pPar, paAtt, paPar, dMax, p);
+    public Archer(String n, int pV, int dA, int pPar, int paAtt, int paPar, int distAttMax, Point2D p, int nbFleches) {
+        super(n, pV, dA, pPar, paAtt, paPar, distAttMax, p);
         this.nbFleches = nbFleches;
     }
 
@@ -29,6 +32,24 @@ public class Archer extends Personnage {
         return nbFleches;
     }
 
+    @Override
+    public void combattre(Creature c) {
+    Random rand = new Random();
+    float distance = this.pos.distanceTo(c.getPos());
+
+    if (distance > 1 && distance <= this.getDistAttMax()) {  // Combat à distance
+        int jetAttaque = rand.nextInt(100) + 1;
+        if (jetAttaque <= this.pageAtt) {  // Attaque réussie
+            c.setPtVie(c.getPtVie() - this.degAtt);
+            System.out.println("L'archer tire et touche " + c.getNom() + ", infligeant " + this.degAtt + " dégâts.");
+        } else {
+            System.out.println("L'archer rate son tir sur " + c.getNom() + ".");
+        }
+    } else {
+        System.out.println("La cible est hors de portée pour un tir à distance.");
+    }
+} 
+    
     public void setNbFleches(int nbFleches) {
         this.nbFleches = nbFleches;
     }
